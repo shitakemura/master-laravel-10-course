45006c5f54ef11db1e0e3695242a58c4bc719e5d
shitakemura <shitakemura@gmail.com>
2023-12-28 22:51:52 +0900

    #1 - blade directives

--- l10-task-list/resources/views/index.blade.php
+++ l10-task-list/resources/views/index.blade.php
@@ -1,7 +1,13 @@
-<div>
-    Hello I'm a blade template!
-</div>
+<h1>
+    The list of tasks
+</h1>
 
-@isset($name)
-    <div>The name is: {{ $name }}</div>
-@endisset
+<div>
+    @forelse ($tasks as $task)
+        <div>
+            <a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{ $task->title }}</a>
+        </div>
+    @empty
+        <div>There are no tasks!</div>
+    @endforelse
+</div>
