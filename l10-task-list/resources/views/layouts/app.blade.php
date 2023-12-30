9df570d5324f42b6e93516e361626f705dec3de6
shitakemura <shitakemura@gmail.com>
2023-12-30 15:13:07 +0900

    #1 - Adding Styling with Tailwind CSS

--- l10-task-list/resources/views/layouts/app.blade.php
+++ l10-task-list/resources/views/layouts/app.blade.php
@@ -3,11 +3,12 @@
 
 <head>
     <title>Laravel 10 Task List App</title>
+    <script src="https://cdn.tailwindcss.com"></script>
     @yield('styles')
 </head>
 
-<body>
-    <h1>@yield('title')</h1>
+<body class="container mx-auto mt-10 mb-10 max-w-lg">
+    <h1 class="text-2xl mb-4">@yield('title')</h1>
     <div>
         @if (session()->has('success'))
             <div>{{ session('success') }}</div>
