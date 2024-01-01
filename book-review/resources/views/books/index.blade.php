e81792ee5aeb023aa870970b3e02e961b439ad15
shitakemura <shitakemura@gmail.com>
2024-01-01 22:13:13 +0900

    #2 - Popular or Highest Rated - the Logic

--- book-review/resources/views/books/index.blade.php
+++ book-review/resources/views/books/index.blade.php
@@ -17,7 +17,7 @@
                 'pupular_last_month' => 'Popular Last Month',
                 'popular_last_6months' => 'Popular Last 6 Months',
                 'highest_rated_last_month' => 'Highest Rated Last Month',
-                'highest_rated_last_6mohths' => 'Highest Rated Last 6 Months',
+                'highest_rated_last_6months' => 'Highest Rated Last 6 Months',
             ];
         @endphp
 
