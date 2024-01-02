f1e8fbfb8698752c94700fb0d76b45a734c0135e
shitakemura <shitakemura@gmail.com>
2024-01-02 14:48:40 +0900

    #2 - Cache and Caching Queries

--- book-review/app/Http/Controllers/BookController.php
+++ book-review/app/Http/Controllers/BookController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Book;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class BookController extends Controller
 {
@@ -28,7 +29,9 @@ class BookController extends Controller
             default => $books->latest(),
         };
 
-        $books = $books->get();
+        // $books = $books->get();
+        $cacheKey = 'books:' . $filter . ':' . $title;
+        $books = cache()->remember($cacheKey, 3600, fn () => $books->get());
 
         return view('books.index', ['books' => $books]);
     }
