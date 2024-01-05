6096110eac0e5625ff294a1735337c036064e618
shitakemura <shitakemura@gmail.com>
2024-01-05 21:42:37 +0900

    #3 - Loading Attendee Relations

--- event-management/app/Http/Traits/CanLoadRelationships.php
+++ event-management/app/Http/Traits/CanLoadRelationships.php
@@ -5,13 +5,14 @@ namespace App\Http\Traits;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Query\Builder as QueryBuilder;
 use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 trait CanLoadRelationships
 {
     public function loadRelationships(
-        Model | QueryBuilder | EloquentBuilder $for,
+        Model | QueryBuilder | EloquentBuilder| HasMany $for,
         ?array $relations = null
-    ) {
+    ): Model | QueryBuilder | EloquentBuilder | HasMany {
         $relations = $relations ?? $this->relations ?? [];
 
         foreach ($relations as $relation) {
