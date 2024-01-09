939dd993bf1f0fa96a7203e3cc12d52b253e5b43
shitakemura <shitakemura@gmail.com>
2024-01-10 00:02:35 +0900

    #4 - Actions in Livewire

--- livewire-poll/resources/views/livewire/create-poll.blade.php
+++ livewire-poll/resources/views/livewire/create-poll.blade.php
@@ -6,5 +6,17 @@
         <input type="text" wire:model.live="title" />
 
         Current title: {{ $title }}
+
+        <div class="mt-4 mb-4">
+            <button class="btn" wire:click.prevent="addOption">Add Option</button>
+        </div>
+
+        <div>
+            @foreach ($options as $index => $option)
+                <div>
+                    {{ $index }} - {{ $option }}
+                </div>
+            @endforeach
+        </div>
     </form>
 </div>
