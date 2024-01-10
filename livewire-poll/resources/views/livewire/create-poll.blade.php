d2b6015c3b63de2708c00a6eed4571d64e3852a2
shitakemura <shitakemura@gmail.com>
2024-01-11 08:30:10 +0900

    #4 - Validation in Livewire

--- livewire-poll/resources/views/livewire/create-poll.blade.php
+++ livewire-poll/resources/views/livewire/create-poll.blade.php
@@ -5,6 +5,10 @@
         {{-- MEMO: https://livewire.laravel.com/docs/components#binding-inputs-to-properties --}}
         <input type="text" wire:model.live="title" />
 
+        @error('title')
+            <div class="text-red-500">{{ $message }}</div>
+        @enderror
+
         <div class="mt-4 mb-4">
             <button class="btn" wire:click.prevent="addOption">Add Option</button>
         </div>
@@ -14,9 +18,13 @@
                 <div class="mb-4">
                     <label>Option {{ $index + 1 }}</label>
                     <div class="flex gap-2">
-                        <input type="text" wire:model="options.{{ $index }}" />
+                        <input type="text" wire:model.live="options.{{ $index }}" />
                         <button class="btn" wire:click.prevent="removeOption({{ $index }})">Remove</button>
                     </div>
+
+                    @error("options.{$index}")
+                        <div class="text-red-500">{{ $message }}</div>
+                    @enderror
                 </div>
             @endforeach
         </div>
