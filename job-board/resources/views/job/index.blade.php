4b2a5810421f87be28980c17734c6deadf6dd585
shitakemura <shitakemura@gmail.com>
2024-01-14 14:56:38 +0900

    #4 - Filtering Jobs: Radio Button Filters(Select One Option of Many)

--- job-board/resources/views/job/index.blade.php
+++ job-board/resources/views/job/index.blade.php
@@ -15,7 +15,29 @@
                         <x-text-input name="max_salary" value="{{ request('max_salary') }}" placeholder="To" />
                     </div>
                 </div>
-                <div>3</div>
+                <div>
+                    <div class="mb-1 font-semibold">Experience</div>
+
+                    <label for="experience" class="mb-1 flex items-center">
+                        <input type="radio" name="experience" value="" @checked(!request('experience`')) />
+                        <span class="ml-2">All</span>
+                    </label>
+
+                    <label for="experience" class="mb-1 flex items-center">
+                        <input type="radio" name="experience" value="entry" @checked(request('experience') === 'entry') />
+                        <span class="ml-2">Entry</span>
+                    </label>
+
+                    <label for="experience" class="mb-1 flex items-center">
+                        <input type="radio" name="experience" value="intermediate" @checked(request('experience') === 'intermediate') />
+                        <span class="ml-2">Intermediate</span>
+                    </label>
+
+                    <label for="experience" class="mb-1 flex items-center">
+                        <input type="radio" name="experience" value="senior" @checked(request('experience') === 'senior') />
+                        <span class="ml-2">Senior</span>
+                    </label>
+                </div>
                 <div>4</div>
             </div>
 
