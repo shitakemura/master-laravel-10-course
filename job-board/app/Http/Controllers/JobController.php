5830fe1b3a4e34e61f9cac9fa1ce17419d8d3438
shitakemura <shitakemura@gmail.com>
2024-01-17 21:37:17 +0900

    #4 - Employer: Other Employer Jobs on the Job Page

--- job-board/app/Http/Controllers/JobController.php
+++ job-board/app/Http/Controllers/JobController.php
@@ -47,7 +47,10 @@ class JobController extends Controller
      */
     public function show(Job $job)
     {
-        return view('job.show', ['job' => $job->load('employer')]);
+        return view(
+            'job.show',
+            ['job' => $job->load('employer.jobs')]
+        );
     }
 
     /**
