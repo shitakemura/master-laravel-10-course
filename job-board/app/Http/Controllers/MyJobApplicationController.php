43dfe65c2b5c1dafb6c4f1943c7542f147307ba1
shitakemura <shitakemura@gmail.com>
2024-01-20 22:14:13 +0900

    #4 - My Applications: Applicant Count, Average Asking Salary

--- job-board/app/Http/Controllers/MyJobApplicationController.php
+++ job-board/app/Http/Controllers/MyJobApplicationController.php
@@ -15,7 +15,11 @@ class MyJobApplicationController extends Controller
             'my_job_application.index',
             [
                 'applications' => auth()->user()->jobApplications()
-                    ->with('job', 'job.employer')
+                    ->with([
+                        'job' => fn ($query) => $query->withCount('jobApplications')
+                            ->withAvg('jobApplications', 'expected_salary'),
+                        'job.employer'
+                    ])
                     ->latest()->get()
             ]
         );
